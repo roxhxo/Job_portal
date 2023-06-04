e9480ec64ecbc0adf65a5f5484c27efe5c75c502
roxhxo <113303820+roxhxo@users.noreply.github.com>
2023-06-04 13:10:01 +0530

    Add files via upload

---
+++ static/javascript/job_page.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $('#job_details').dataTable();
+});
---
+++ static/javascript/login.js
@@ -0,0 +1,4 @@
+function show(x,y){
+    document.getElementById(x).style.display = "block";
+    document.getElementById(y).style.display = "none";
+    }
